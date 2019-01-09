a37ac1f8e1756bc48f2589a8df09e4148071d247
AndresAngel <afa_angel@hotmail.com>
2019-01-09 01:15:41 -0300

    E15CP2A1 pictorydevise

--- app/models/history.rb
+++
@@ -1,3 +0,0 @@
-class History < ApplicationRecord
-  mount_uploader :picture, ImageUploader
-end
---
+++ pictory2/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :histories
+  validates :username, uniqueness: true, presence: true
+  validates :name, presence: true
+end
